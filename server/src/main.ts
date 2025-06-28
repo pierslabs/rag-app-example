38bb9c6417625af40a11513ad572743dc6fec56a
Pedro Losas <pedrolosasp@gmail.com>
2025-06-28 12:09:07 +0200

    wip:rag-app add logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Server is running on port ${process.env.PORT ?? 3000}`);
 }
+
 bootstrap();
